bd66f0e4183335d6e253faab545b9f887ee5c22d
Ruth Ivimey-Cook <ruth@ivimey.org>
2018-01-17 22:26:15 +0000

    Remove extraneous CR; reindent.

--- src/config/config.facets.php
+++ src/config/config.facets.php
@@ -1,31 +1,100 @@
 <?php
 // do not config here, this config file will be overwritten by Thesaurus and Ontologies Manager
 
-$cfg['facets']['author_s'] = array ('label'=>'Author', 'facet_limit'=>'10', 'snippets_limit'=>'10','snippets_enabled'=>true);
+$cfg['facets']['author_s'] = array(
+  'label' => 'Author',
+  'facet_limit' => '10',
+  'snippets_limit' => '10',
+  'snippets_enabled' => TRUE
+);
 
-$cfg['facets']['tag_ss'] = array ('label'=>'Tags', 'facet_limit'=>'10', 'snippets_limit'=>'10','snippets_enabled'=>true);
+$cfg['facets']['tag_ss'] = array(
+  'label' => 'Tags',
+  'facet_limit' => '10',
+  'snippets_limit' => '10',
+  'snippets_enabled' => TRUE
+);
 
-$cfg['facets']['person_ss'] = array ('label'=>'Persons', 'facet_limit'=>'10', 'snippets_limit'=>'10','snippets_enabled'=>true);
+$cfg['facets']['person_ss'] = array(
+  'label' => 'People',
+  'facet_limit' => '10',
+  'snippets_limit' => '10',
+  'snippets_enabled' => TRUE
+);
 
-$cfg['facets']['organization_ss'] = array ('label'=>'Organizations', 'facet_limit'=>'10', 'snippets_limit'=>'10','snippets_enabled'=>true);
+$cfg['facets']['organization_ss'] = array(
+  'label' => 'Organizations',
+  'facet_limit' => '10',
+  'snippets_limit' => '10',
+  'snippets_enabled' => TRUE
+);
 
-$cfg['facets']['location_ss'] = array ('label'=>'Location', 'facet_limit'=>'10', 'snippets_limit'=>'10','snippets_enabled'=>true);
+$cfg['facets']['location_ss'] = array(
+  'label' => 'Location',
+  'facet_limit' => '10',
+  'snippets_limit' => '10',
+  'snippets_enabled' => TRUE
+);
 
-$cfg['facets']['language_s'] = array ('label'=>'Language', 'facet_limit'=>'10', 'snippets_limit'=>'10','snippets_enabled'=>false);
+$cfg['facets']['language_s'] = array(
+  'label' => 'Language',
+  'facet_limit' => '10',
+  'snippets_limit' => '10',
+  'snippets_enabled' => FALSE
+);
 
-$cfg['facets']['email_ss'] = array ('label'=>'Email', 'facet_limit'=>'10', 'snippets_limit'=>'10','snippets_enabled'=>true);
+$cfg['facets']['email_ss'] = array(
+  'label' => 'Email',
+  'facet_limit' => '10',
+  'snippets_limit' => '10',
+  'snippets_enabled' => TRUE
+);
 
-$cfg['facets']['message_from_ss'] = array ('label'=>'Message from', 'facet_limit'=>'10', 'snippets_limit'=>'10','snippets_enabled'=>true);
+$cfg['facets']['message_from_ss'] = array(
+  'label' => 'Message from',
+  'facet_limit' => '10',
+  'snippets_limit' => '10',
+  'snippets_enabled' => TRUE
+);
 
-$cfg['facets']['message_to_ss'] = array ('label'=>'Message to', 'facet_limit'=>'10', 'snippets_limit'=>'10','snippets_enabled'=>true);
+$cfg['facets']['message_to_ss'] = array(
+  'label' => 'Message to',
+  'facet_limit' => '10',
+  'snippets_limit' => '10',
+  'snippets_enabled' => TRUE
+);
 
-$cfg['facets']['message_cc_ss'] = array ('label'=>'Message CC', 'facet_limit'=>'10', 'snippets_limit'=>'10','snippets_enabled'=>true);
+$cfg['facets']['message_cc_ss'] = array(
+  'label' => 'Message CC',
+  'facet_limit' => '10',
+  'snippets_limit' => '10',
+  'snippets_enabled' => TRUE
+);
 
-$cfg['facets']['message_bcc_ss'] = array ('label'=>'Message BCC', 'facet_limit'=>'10', 'snippets_limit'=>'10','snippets_enabled'=>true);
+$cfg['facets']['message_bcc_ss'] = array(
+  'label' => 'Message BCC',
+  'facet_limit' => '10',
+  'snippets_limit' => '10',
+  'snippets_enabled' => TRUE
+);
 
-$cfg['facets']['hashtag_ss'] = array ('label'=>'Hashtags', 'facet_limit'=>'10', 'snippets_limit'=>'10','snippets_enabled'=>true);
+$cfg['facets']['hashtag_ss'] = array(
+  'label' => 'Hashtags',
+  'facet_limit' => '10',
+  'snippets_limit' => '10',
+  'snippets_enabled' => TRUE
+);
 
-$cfg['facets']['content_type_group'] = array ('label'=>'Content type group', 'facet_limit'=>'10', 'snippets_limit'=>'10','snippets_enabled'=>false);
+$cfg['facets']['content_type_group'] = array(
+  'label' => 'Content type group',
+  'facet_limit' => '10',
+  'snippets_limit' => '10',
+  'snippets_enabled' => FALSE
+);
 
-$cfg['facets']['content_type'] = array ('label'=>'Content type', 'facet_limit'=>'10', 'snippets_limit'=>'10','snippets_enabled'=>false);
-?>
+$cfg['facets']['content_type'] = array(
+  'label' => 'Content type',
+  'facet_limit' => '10',
+  'snippets_limit' => '10',
+  'snippets_enabled' => FALSE
+);
