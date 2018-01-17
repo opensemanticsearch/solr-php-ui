dbbc93f02e77530bfd2dbafd078d82a399692d64
Ruth Ivimey-Cook <ruth@ivimey.org>
2018-01-17 20:56:48 +0000

    Use template-php and simplify logic.

--- src/templates/view.images.php
+++ src/templates/view.images.php
@@ -1,14 +1,13 @@
 <?php
 // Standard view
 //
-// Show results as list
-
+// Show results as images
 ?>
 
 <div id="results" class="row">
   <ul class="small-block-grid-1 medium-block-grid-2 large-block-grid-3">
-    <?php
-    foreach ($results->response->docs as $doc) {
+
+    <?php foreach ($results->response->docs as $doc):
 
       // URI
       if (isset($doc->container_s)) {
@@ -18,12 +17,11 @@
         $id = $doc->id;
       }
 
-
       $uri_label = $id;
       $uri_tip = FALSE;
 
       // if file:// then only filename
-      if (strpos($id, "file://") == 0) {
+      if (strpos($id, 'file://') === 0) {
         $uri_label = htmlspecialchars(basename($id));
         // for tooptip remove file:// from beginning
         $uri_tip = htmlspecialchars(substr($id, 7));
@@ -35,24 +33,20 @@
       // Title
       $title = FALSE;
 
-      if (isset($doc->title)) {
-        if (!empty($doc->title)) {
-          $title = htmlspecialchars($doc->title);
-        }
+      if (!empty($doc->title)) {
+        $title = htmlspecialchars($doc->title);
       }
 
       // Type
       $type = $doc->content_type;
 
       // Modified date
+      $datetime = FALSE;
       if (isset($doc->file_modified_dt)) {
         $datetime = $doc->file_modified_dt;
       }
       elseif (isset($doc->last_modified)) {
         $datetime = $doc->last_modified;
-      }
-      else {
-        $datetime = FALSE;
       }
 
       // File size
@@ -72,16 +66,13 @@
         if (strlen($snippet) > $snippetsize) {
           $snippet = substr($snippet, 0, $snippetsize) . "...";
           $snippet = htmlspecialchars($snippet);
-
         }
-      }
-      ?>
+      } ?>
+
       <li>
         <div class="image">
           <a target="_blank" href="<?= $id ?>">
-            <img width="200" src="<?= $id ?>" <?php if ($title) {
-              echo 'title="' . $title . '"';
-            } ?> />
+            <img width="200" src="<?= $id ?>" <?= $title ? 'title="' . $title . '"' : '' ?> />
           </a>
         </div>
 
@@ -90,21 +81,12 @@
           <div class="size small-4 columns"><?= $file_size_txt ?></div>
         </div>
 
-        <?php if ($author) {
-          print '<div class="author">' . $author . '</div>';
-        } ?>
+        <?php if ($author): ?>
+          <div class="author">$author</div>'
+        <?php endif; ?>
 
         <div class="title imagelist">
-
-          <a target="_blank" href="<?= $id ?>"><h2>
-              <?php if ($title) { ?>
-                <?= $title ?>
-              <?php } else { ?>
-                <?= $uri_label ?>
-              <?php } ?>
-            </h2>
-          </a>
-
+          <a href="<?= $id ?>"><h2><?= $title ? $title : $uri_label ?></h2></a>
         </div>
 
         <div class="snippet">
@@ -112,23 +94,23 @@
         </div>
 
         <div class="commands">
-          <?php if ($uri_tip) { ?>
-          <span data-tooltip class="has-tip" title="<?= $uri_tip ?>">
-				<?php } ?>
-            <a target="_blank" href="<?= $id ?>"><?php echo t('open'); ?></a>
 
+          <?php if ($uri_tip): ?>
+            <span data-tooltip class="has-tip" title="<?= $uri_tip ?>">
+              <a href="<?= $id ?>"><?= t('open'); ?></a>
+            </span>
+          <?php else: ?>
+            <a href="<?= $id ?>"><?= t('open'); ?></a>
+          <?php endif; ?>
 
-            <?php if ($uri_tip) { ?>
-					</span>
-        <?php } ?>
-          <?php if ($cfg['metadata']['server']) { ?> | <a target="_blank"
-                                                          title="<?php echo t('meta description'); ?>"
-                                                          href="<?php print get_metadata_uri($cfg['metadata']['server'], $id); ?>"><?php echo t('meta'); ?></a> <?php } ?>
-          | <?php print '<a target="_blank" href="preview.php?id=' . urlencode($id) . '">' . t('Preview') . '</a>'; ?>
+          <?php if ($cfg['metadata']['server']): ?>
+            | <a title="<?= t('meta description'); ?>"
+                 href="<?= get_metadata_uri($cfg['metadata']['server'], $id); ?>"><?= t('meta'); ?></a>
+          <?php endif; ?>
+          | <?= '<a href="preview.php?id=' . urlencode($id) . '">' . t('Preview') . '</a>'; ?>
+
         </div>
       </li>
-      <?php
-    } // foreach doc
-    ?>
+    <?php endforeach; ?>
   </ul>
 </div>
