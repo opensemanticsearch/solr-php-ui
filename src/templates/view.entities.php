312f33d4a8f8d8d281195db6cfef617862ab3d87
Markus Mandalka <github@mandalka.name>
2019-10-10 15:08:18 +0200

    Entities view: Support taxonomies

--- src/templates/view.entities.php
+++ src/templates/view.entities.php
@@ -14,8 +14,19 @@
 
       <div class="small-12 medium-6 columns">
 
-        <?php print_facet($results, $facet, t($facet_config['label']), $facets_limit, $view='entities'); ?>
+        <?php
+        
+			if ( isset($facet_config['pathfacet']) ) {
+				$path = FALSE;
+				if (isset($facet_config['path'])) {
+					$path=$facet_config['path'];
+				}
+	      	print_facet($results, $facet_config['pathfacet'], t($facet_config['label']), $facets_limit, 'entities', $facet, $path);
+   	   } else {
+      		print_facet($results, $facet, t($facet_config['label']), $facets_limit, 'entities');
+      	}
 
+		  ?>
       </div>
 
     <?php endif; ?>
